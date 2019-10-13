db72e725965c50f798da781f753e48b62d44992c
Michelle Barker <mbarker_84@hotmail.co.uk>
2019-10-13 15:09:40 +0100

    Add imagemin plugin

---
+++ imagemin.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 10 },
+  "mozjpeg": { "progressive": true, "quality": 10 },
+  "pngquant": { "quality": [0.25, 0.5] },
+  "svgo": {
+    "plugins": [
+      { "removeViewBox": false },
+      { "cleanupIDs": true },
+    ]
+  },
+  "webp": { "quality": 10 }
+}
